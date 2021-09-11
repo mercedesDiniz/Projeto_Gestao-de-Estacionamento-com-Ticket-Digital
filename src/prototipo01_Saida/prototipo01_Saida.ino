16c09c1d3df8287df00ef8be51c05a5c3b471b1d
mercedesDiniz <mercy.diniz82@gmail.com>
2021-09-11 17:33:26 -0300

    Organização para o Prototipo da Saida + Pesquisa PIR

---
+++ src/prototipo01_Saida/prototipo01_Saida.ino
@@ -0,0 +1,15 @@
+/* PROTOTIPO (SAIDA) 
+Link: 
+
+# REFERENCIAS:
+- Bibliotecas:
+    1. Para gerar Qrcode: qrcode (Autor: Richard Moore)
+    2. Para o Display: Adafruit_SSD1306 (Autor: Adafruit)
+    3. Para o Módulo RTC ds1307: RTClib (Autor: Adafruit) 
+
+- Modelos Base:
+    1. Dynamic QRcode on ssd1306-big-sram.ino (https://wokwi.com/arduino/projects/301360621680591373)
+    2. ds1307-basic.ino (https://wokwi.com/arduino/projects/305979285237137984)
+*/
+
+
